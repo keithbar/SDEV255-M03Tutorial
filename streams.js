44f1ac9eb0887932f8fd11d9b67fc6225f93738f
Keith <kbarber34@ivytech.edu>
2024-02-05 15:54:30 -0500

    ready for submission

--- streams.js
+++ streams.js
@@ -4,12 +4,12 @@ const readStream = fs.createReadStream("./docs/blog3.txt", {encoding: "utf8"});
 const writeStream = fs.createWriteStream("./docs/blog4.txt");
 
 //event listener
-/* readStream.on("data", (chunk) => {
+readStream.on("data", (chunk) => {
     console.log("------NEW CHUNK------");
     console.log(chunk);
     writeStream.write("\nNEW CHUNK\n");
     writeStream.write(chunk);
-}); */
+});
 
 // piping
-readStream.pipe(writeStream);
+//readStream.pipe(writeStream);
